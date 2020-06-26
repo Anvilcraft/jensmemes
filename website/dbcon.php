578c7b9df8fd362e9d0baf8712733a739283f134
Timo Ley <timo@leyt.xyz>
2020-06-26 21:15:34 +0200

    fail

--- website/dbcon.php
+++ website/dbcon.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","jensmemes","aCSw24pHYOjYSzeV","jensmemes");
+$con = mysqli_connect("localhost","jensmemes","PASSWORT","jensmemes");
 
 // Check connection
 if (mysqli_connect_errno())
