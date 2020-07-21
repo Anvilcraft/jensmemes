8d65bdc44677a9b282e938f4511254d3f8d81ca0
ITbyHF <hoefler.alec@borg-kindberg.at>
2020-07-21 23:00:11 +0200

    Upload

---
+++ website-neu/incl/uploadOK.php
@@ -0,0 +1,22 @@
+<?php
+include('../dbcon.php');
+
+
+$tokencookie = $_COOKIE['token'];
+$resulttoken=mysqli_query($con, "select * from token WHERE token='$tokencookie'")or die('Error In Session');
+$rowtoken=mysqli_fetch_array($resulttoken);
+
+$tokenpost = $_POST['token'];
+$resultpost=mysqli_query($con, "select * from token WHERE token='$tokenpost'")or die('Error In Session');
+$rowpost=mysqli_fetch_array($resultpost);
+
+
+if(!empty($rowtoken) || !empty($rowpost)){
+  $uploadOK = true;
+  //echo "<b>Authenticated.</b>";
+}else{
+  $uploadOK = false;
+  echo "<b>unauthenticated.</b>";
+}
+
+ ?>
---
+++ website-neu/random.php
@@ -0,0 +1,12 @@
+<?php
+$files = glob("images/*/*");
+$random = rand(0, count($files) - 1);
+$file = $files[$random];
+$ext = strtolower(pathinfo($file, PATHINFO_EXTENSION));
+if ($ext == "mp4") {
+echo "<a href='$file'><video src='$file' controls ></video></a>";
+} else {
+echo "<a href='$file'><img src='$file' /></a>";
+}
+?>
+
