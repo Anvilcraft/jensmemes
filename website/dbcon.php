e9d5cac9b8e9262b0e7336c9e80da7af803c3386
ITbyHF <hoefler.alec@borg-kindberg.at>
2020-07-21 23:05:36 +0200

    Jetzt happy tilera=

--- website/dbcon.php
+++
@@ -1,9 +0,0 @@
-<?php
-$con = mysqli_connect("localhost","jensmemes","PASSWORT","jensmemes");
-
-// Check connection
-if (mysqli_connect_errno())
-  {
-  echo "Failed to connect to MySQL: " . mysqli_connect_error();
-  }
-?>
