268566044d56f1a6578abfdc5a48ae44dad30b5e
ITbyHF <61453797+ITbyHF@users.noreply.github.com>
2020-08-02 19:08:54 +0200

    Created dbcon.php

---
+++ website/dbcon.php
@@ -0,0 +1,11 @@
+<?php
+
+include "config.php";
+$con = mysqli_connect(SQLHost, SQLUser, SQLPasswort, SQLDatenbank);
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "<b style='color: #9c4b4b; text-decoration: underline;'>Failed to connect to <i >" .$SQLHost."</i> </b> :: " . mysqli_connect_error();
+  }
+?>
