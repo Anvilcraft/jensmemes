f2ac87f8117901157e7bff1d8c5850b877c28663
ITbyHF <61453797+ITbyHF@users.noreply.github.com>
2020-08-02 23:17:43 +0200

    Create dbcon_beispielfuertilera.php
    Jetzt happy?

---
+++ website/dbcon_beispielfuertilera.php
@@ -0,0 +1,12 @@
+<?php
+
+//Rename this File to dbcon.php, then it will work properly
+
+$con = mysqli_connect("SQLHost", "SQLUser", "SQLPasswor"t, "SQLDatenbank");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "<b style='color: #9c4b4b; text-decoration: underline;'>Failed to connect to <i >" .$SQLHost."</i> </b> :: " . mysqli_connect_error();
+  }
+?>
