fead4824ab68aef457d24318ff6651841c7713a7
ITbyHF <61453797+ITbyHF@users.noreply.github.com>
2020-08-12 23:55:29 +0200

    Update bot.py

--- Bot/bot.py
+++ Bot/bot.py
@@ -7,8 +7,7 @@ from hashlib import md5
 import random
 import string
 
-bot = commands.Bot(command_prefix=prefix)
-@commands.cooldown(1, 30, commands.BucketType.user)
+bot = commands.Bot(command_prefix=prefix
 @bot.event #print the username and id to the console and change the game status
 async def on_ready():
     print('Logged in as')
