2c6914ecdf203f018fa75f950fce8bcb087e170b
Timo Ley <timo@leyt.xyz>
2020-08-13 14:02:25 +0200

    Port random.php to new api

--- website/randomapi.php
+++ website/randomapi.php
@@ -1,6 +1,3 @@
 <?php
-$files = glob("images/*/*");
-$random = rand(0, count($files) - 1);
-$file = $files[$random];
-echo "https://jensmemes.tilera.xyz/" . $file;
+echo "Deprecated API. Use: https://jensmemes.tilera.xyz/api.php/random"
 ?>
