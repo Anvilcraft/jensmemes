7375e9ed3f33c05d444373075b6cfec2eaf017e9
ITbyHF <61453797+ITbyHF@users.noreply.github.com>
2020-09-02 22:46:50 +0200

    deleted unnecessary file

--- website/incl/clientIP.php
+++
@@ -1,9 +0,0 @@
-<?php
-$clientIP =$_SERVER['REMOTE_ADDR'];
-$tk = $_COOKIE['token'];
-$sql = "UPDATE `token` SET `latest-ip` = '$clientIP' WHERE `token` = '$tk';
-";
-if(!mysqli_query($con, $sql)){
-    echo "Err_ Sending Client IP".mysqli_error($con);
-}
-?>
